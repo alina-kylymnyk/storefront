ce8f651d7fff171778b7c933944f6feeff1cfb13
alina-kylymnyk <kylymnyk.projects@gmail.com>
2025-08-07 18:46:14 +0300

    add settings configuratios

--- storefront/storefront/settings/common.py
+++ storefront/storefront/settings/common.py
@@ -169,7 +169,7 @@ EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 2525
 
-CELERY_BROKER_URL = "redis://localhost:6379/1"
+
 CELERY_BEAT_SCHEDULE = {
     "notify_customers": {
         "task": "playground.tasks.notify_customers",
@@ -179,16 +179,7 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
-        "TIMEOUT": 10 * 60,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
-    }
-}
+
 
 LOGGING = {
     "version": 1,
